b5bed489103382d998e4e1ca862d1640669ae138
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-05-14 20:48:45 +0530

    Pagination First Commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [paginationNum, setPaginationNum] = useState(0)
+  const [employees, setEmployees] = useState([]);
+  const URL = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setEmployees(data))
+      .catch((err) => console.log('failed to fetch data'));
+  }, []);
+
+  const slicedData = (num,end) => {
+    return (
+      employees.slice(num,end).map(employee => {
+        return (<tr key={employee.id}>
+          <td>{employee.id}</td>
+          <td>{employee.name}</td>
+          <td>{employee.email}</td>
+          <td>{employee.role}</td>
+        </tr>)
+      })
+    )
+  }
+  console.log(paginationNum,Math.ceil(employees.length / 10))
+  return (
+    <>
+      <h1>Employee Data Table</h1>
+      <table className="customTable">
+        <thead>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </thead>
+        <tbody>
+          {slicedData(paginationNum * 10,paginationNum * 10 + 10)}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        {paginationNum > 0 && <button onClick={() => setPaginationNum(prev => prev - 1)}>Previous</button>}
+        <button>{paginationNum + 1}</button>
+        {paginationNum + 1 <= Math.ceil(employees.length / 10) - 1 && <button onClick={() => setPaginationNum(prev => prev + 1)}>Next</button>}
+      </div>
+    </>
+  );
+}
+
+export default App;
